253fec2e9e78dcac97b19db15aea2fe745954ec6
Andrei Vilcu <andrei3002002@gmail.com>
2023-05-05 19:47:13 +0200

    Klass Human

---
+++ src/Human.java
@@ -0,0 +1,9 @@
+// класс, файл имеет название Human.java
+public class Human {
+
+  // fields (поля, свойства, характеристики)
+  String name; // имя
+  double age; // возраст
+  int steps; // количество шагов
+  String eyesColor;
+}
