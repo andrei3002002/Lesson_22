ff3c692926f2e8fa85d02a8de3a92c9918fa0bdd
Andrei Vilcu <andrei3002002@gmail.com>
2023-05-07 21:11:10 +0200

    New class and constructor

---
+++ src/Home_Work_20/Home_Work_20.java
@@ -0,0 +1,19 @@
+package Home_Work_20;
+
+public class Home_Work_20 {
+  private double radius;
+  private double center_x;
+  private double center_y;
+
+  public  Home_Work_20(){
+    this(1.0, 0.0, 0.0);
+  }
+
+  public Home_Work_20 (double radius, double center_x, double center_y){
+    this.radius = radius;
+    this.center_x = center_x;
+    this.center_y = center_y;
+  }
+
+
+}
